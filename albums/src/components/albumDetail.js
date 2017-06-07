33ff12eb3fee7a1548afa2528e08c2ddefcd5e6d
ShahzaibSE <shahzaibanalyst@outlook.com>
2017-06-07 18:21:38 +0500

    Image component Implementation

--- albums/src/components/albumDetail.js
+++ albums/src/components/albumDetail.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 //Reusable Component 'Card' and 'Card Section'
 import Card from './card';
 import CardSection from './cardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { thumnbnailStyle, headerStyleContent } = styles;
+    console.log('Album');
+    console.log(album.thumbnail_image);
+
     return (
         <Card>
             <CardSection>
-              <Text>{props.album.title}</Text>
+                <View>
+                 <Image style={thumnbnailStyle} source={{ uri: thumbnail_image }} />
+                </View>
+                <View style={headerStyleContent}>
+                 <Text>{title}</Text>
+                 <Text>{artist}</Text>
+                </View>
               </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerStyleContent: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumnbnailStyle: {
+        height: 50,
+        width: 50
+    } 
 };
 
 export default AlbumDetail;
